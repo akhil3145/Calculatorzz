f1ca7a91b304491e62ebba38aec24279ab47dee1
akhil3145 <akhilpandey1000@gmail.com>
2026-06-13 22:28:30 +0530

    separated script.js from index.html

---
+++ script.js
@@ -0,0 +1,30 @@
+const display = document.querySelector('input[name="display"]');
+const buttons = document.querySelectorAll('input[type="button"]');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.value;
+
+        // Clear display if it shows 'Error' from a previous invalid calculation
+        if (display.value === 'Error') {
+            display.value = '';
+        }
+
+        if (value === 'AC') {
+            display.value = '';
+        } else if (value === 'DE') {
+            display.value = display.value.toString().slice(0, -1);
+        } else if (value === '=') {
+            try {
+                // Safeguard eval in case of bad mathematical input
+                if (display.value) {
+                    display.value = eval(display.value);
+                }
+            } catch (error) {
+                display.value = 'Error';
+            }
+        } else {
+            display.value += value;
+        }
+    });
+});
